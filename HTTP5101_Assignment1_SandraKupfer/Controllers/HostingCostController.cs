55520bd8aefa36580ef422095f9532d4e4a1b06c
Sandra Kupfer <kupfer.sandra.m@gmail.com>
2020-09-24 14:47:51 -0400

    Fixed currency formatting (yay!) and removed unnecessary using statements.

--- HTTP5101_Assignment1_SandraKupfer/Controllers/HostingCostController.cs
+++ HTTP5101_Assignment1_SandraKupfer/Controllers/HostingCostController.cs
@@ -1,23 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System.Collections.Generic;
 using System.Web.Http;
 
 namespace HTTP5101_Assignment1_SandraKupfer.Controllers
 {
     public class HostingCostController : ApiController
     {
-        private static readonly decimal baseCost = 5.50m;
-        private static readonly string moneyFormatString = "{0:.00}";
-        private static readonly string zeroString = "0";
-        private static readonly string emptyString = "";
+        private static readonly decimal baseCost = 5.5m;
 
-        private static readonly string fortnightsSingleMiddle = " fortnight at $" + String.Format(moneyFormatString, baseCost) + "/FN = $";
-        private static readonly string fortnightsMiddle = " fortnights at $" + String.Format( moneyFormatString, baseCost ) + "/FN = $";
-        private static readonly string hstPrefix = "HST 13% = $";
-        private static readonly string totalPrefix = "Total = $";
+        private static readonly string baseCostString = baseCost.ToString("C");
+        private static readonly string fortnightsSingleMiddle = " fortnight at " + baseCostString + "/FN = ";
+        private static readonly string fortnightsMiddle = " fortnights at " + baseCostString + "/FN = ";
+        private static readonly string hstPrefix = "HST 13% = ";
+        private static readonly string totalPrefix = "Total = ";
         private static readonly string currencyString = " CAD";
 
         private readonly string[] hostingCostStrings = new string[3];
@@ -25,7 +19,7 @@ namespace HTTP5101_Assignment1_SandraKupfer.Controllers
         /// <summary>
         /// Assignment 1.bonus
         /// GET http://localhost/api/HostingCost/{id}
-        /// Calculates the hosting cost for the input number of days and return a string summarizing the cost.
+        /// Calculates the hosting cost for the input number of days and return a set of strings summarizing the cost.
         /// </summary>
         /// <param name="id">Number of nights hosted.</param>
         /// <returns>Three strings that describe the base hosting cost, tax, and total.</returns>
@@ -36,17 +30,16 @@ namespace HTTP5101_Assignment1_SandraKupfer.Controllers
             decimal hst = subTotal * 0.13m;
             decimal total = subTotal + hst;
 
-            hostingCostStrings[0] = numFortnights.ToString() 
-                + (numFortnights == 1 ? fortnightsSingleMiddle : fortnightsMiddle ) 
-                + String.Format( moneyFormatString, subTotal ) 
+            hostingCostStrings[0] = numFortnights.ToString()
+                + (numFortnights == 1 ? fortnightsSingleMiddle : fortnightsMiddle)
+                + subTotal.ToString("C")                
                 + currencyString;
-            // TODO: look into composite formatting or something else to get rid of this tertiary statement.
-            // Maybe try ToString with some different format strings.
             hostingCostStrings[1] = hstPrefix 
-                + (numFortnights == 0 ? zeroString : emptyString) 
-                + String.Format(moneyFormatString, hst ) 
+                + hst.ToString("C") 
                 + currencyString;
-            hostingCostStrings[2] = totalPrefix + String.Format(moneyFormatString, total );
+            hostingCostStrings[2] = totalPrefix 
+                + total.ToString("C")
+                + currencyString;
 
             return hostingCostStrings;
         }
